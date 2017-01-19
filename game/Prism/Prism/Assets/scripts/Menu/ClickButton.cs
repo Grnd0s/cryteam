9abf7994ecab776910daa553f04d23b8fd7c511d
atomgenie <atomgenie@me.com>
2017-01-20 00:50:56 +0100

    Next

---
+++ game/Prism/Prism/Assets/scripts/Menu/ClickButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour {
+
+
+
+    public TextColor ltext;
+    bool isClick = false;
+	// Use this for initialization
+	void Start ()
+    {
+        ltext.changeColorToFirst();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void goHead()
+    {
+    }
+}
---
+++ game/Prism/Prism/Assets/scripts/Menu/TextColor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextColor : MonoBehaviour {
+    
+
+    public Color firstColor;
+    public Color lastColor;
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnGUI()
+    {
+        theText = firstColor;
+    }
+
+    public void changeColorToLast()
+    {
+
+    }
+    public void changeColorToFirst()
+    {
+        GUI.color = firstColor;
+    }
+
+    public static implicit operator TextColor(Color v)
+    {
+        throw new NotImplementedException();
+    }
+}
